ba9c78b37e9ae42ad0ba9f826d79dd05c04bfbf8
sabrina-albanese <sabrina.albanese@edu.itspiemonte.it>
2024-12-05 10:35:10 +0100

    upgrade
    Change sprite in dialogue
    Import of a package for timeline use

--- PuntaClicca/Assets/Script/TextCanvas.cs
+++ PuntaClicca/Assets/Script/TextCanvas.cs
@@ -1,15 +1,23 @@
 using System;
+using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextCanvas : MonoBehaviour
 {
     public static Action <ItemScriptableObject, int> OnShowInteractionText;
     public static Action OnHideText;
-    public static Action <string> OnShowYesText;
+    public static Action <ItemScriptableObject> OnShowYesText;
 
     [SerializeField]
     private GameObject Bg_text;
+
+    [SerializeField]
+    private GameObject characterPortrait;
+   
+    [SerializeField]
+    private Sprite mainCharactePortrait;
 
     [SerializeField]
     private TextMeshProUGUI charText;
@@ -18,14 +26,23 @@ public class TextCanvas : MonoBehaviour
         HideText();
         OnShowInteractionText += CheckNumInteractionPhrase;
         OnHideText += HideText;
-        OnShowYesText += ShowText;
+        OnShowYesText += YesText;
     }
 
     private void CheckNumInteractionPhrase(ItemScriptableObject item, int numInteraction){
-        if(numInteraction<item.InteractionPhrases.Count)
+        if(numInteraction<item.InteractionPhrases.Count){
             ShowText(item.InteractionPhrases[numInteraction]);
-        else
+            ChangeCharacterPortrait(item);
+        }
+        else{
             ShowText(item.InteractionPhrases[item.InteractionPhrases.Count-1]);
+            ChangeCharacterPortrait(item);
+        }           
+    }
+
+    private void YesText(ItemScriptableObject item){
+        ShowText(item.yesInteractionPhrase);
+        ChangeCharacterPortrait(item);
     }
 
     private void ShowText(string newText){
@@ -36,5 +53,15 @@ public class TextCanvas : MonoBehaviour
     private void HideText(){
         Bg_text.SetActive(false);
     }
+
+    private void ChangeCharacterPortrait(ItemScriptableObject item) {
+        //if item characterSprite is not null, change sprite with new one
+        if(item.characterSprite != null)
+            characterPortrait.GetComponent<UnityEngine.UI.Image>().sprite = item.characterSprite;
+        else 
+        //if item characterSprite is null, change sprite with main character Sprite
+            characterPortrait.GetComponent<UnityEngine.UI.Image>().sprite = mainCharactePortrait;
+            
+    }   
 
 }
