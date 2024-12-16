47a530cbd5c7513f323c2d3cc353934ddc0add43
sabrina-albanese <sabrina.albanese@edu.itspiemonte.it>
2024-12-16 16:06:41 +0100

    Menu Options and credits working
    Added Audio Sliders to change music and sfx volume

--- PuntaClicca/Assets/Script/MainMenuCanvas.cs
+++ PuntaClicca/Assets/Script/MainMenuCanvas.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class MainMenuCanvas : MonoBehaviour
 {
-    [SerializeField] private GameObject ContinueButton;
+    [SerializeField] private GameObject MainMenu;
+    [SerializeField] private GameObject Credits;
+    [SerializeField] private GameObject Options;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (PlayerPrefs.HasKey("Scene"))
-            ContinueButton.SetActive(true);
-        else
-            ContinueButton.SetActive(false);
+           StartMenu();
+    }
+
+    public void StartMenu()
+    {
+        MainMenu.SetActive(true);
+        Credits.SetActive(false);
+        Options.SetActive(false);
+    }
+    
+    public void CreditsMenu()
+    {
+        MainMenu.SetActive(false);
+        Credits.SetActive(true);
+        Options.SetActive(false);
+    }
+
+    public void OptionsMenu()
+    {
+        MainMenu.SetActive(false);
+        Credits.SetActive(false);
+        Options.SetActive(true);
+    }
+    
+    public void Back()
+    {
+        StartMenu();
     }
     
     public void QuitGame()
